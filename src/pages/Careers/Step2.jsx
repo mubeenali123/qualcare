188b93430a5a7430fcac9acfcd6f44a903416a73
samuelcarterwork-ship-it <samuelcarter.work@gmail.com>
2026-05-24 20:21:38 +0500

    Pushed to Azure

--- src/pages/Careers/Step2.jsx
+++ src/pages/Careers/Step2.jsx
@@ -170,7 +170,7 @@ useEffect(() => {
 
           <div className="form-section">
             <label className="section-label">City and State <span className="required">*</span></label>
-            <div className="address-grid">
+            <div className="address-grid-2">
               <div className="form-field">
                 <input 
                   type="text" 
